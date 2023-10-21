c8ee521441d3f307935bca9761b39baa14c4c4ae
Galib <aagalib2323@gmail.com>
2023-10-21 23:30:56 +0600

    blank page issue fixed

--- src/Components/BrandLinks/Google.jsx
+++ src/Components/BrandLinks/Google.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import ProductCard from "./ProductCard/ProductCard";
+import no from '../../assets/no.png';
 
 const Google = () => {
 
@@ -7,9 +8,11 @@ const Google = () => {
     console.log(products);
     const googleProducts = products.filter(product => product.brandName === 'Google');
     if(googleProducts.length == 0){
-        return <div>
-            <h2>No products available</h2>
-        </div>
+        return <div className="flex flex-col  items-center justify-center min-h-screen">
+        <h2 className="text-4xl font-extrabold text-red-700 mb-8">Oops..</h2>
+        <p className="text-xl font-semibold mb-8">No Products available of this brand</p>
+        <img className="w-56 md:w-96" src={no} alt="" />
+    </div>
     }
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:container mx-auto">
